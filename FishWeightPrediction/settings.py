8c4af8096c4e212fe3b130d205139499e8f2688f
Antoine El Choueiri <64024432+anthonychoueiri@users.noreply.github.com>
2022-12-21 02:39:42 +0200

    fix server error

--- FishWeightPrediction/settings.py
+++ FishWeightPrediction/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kwv5%y6@fz7w#%3+fc27l%guavm7kr%b)ual2llx5lxev-v0o+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
