c9bc89ca172ac1b9b401a11dea5653b4231b321b
Antoine El Choueiri <64024432+anthonychoueiri@users.noreply.github.com>
2022-12-21 02:19:18 +0200

    fix heroku deploy

--- FishWeightPrediction/settings.py
+++ FishWeightPrediction/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kwv5%y6@fz7w#%3+fc27l%guavm7kr%b)ual2llx5lxev-v0o+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- Procfile
+++ Procfile
@@ -1,4 +1,5 @@
 worker.1: redis-server
 worker.2: celery -A PredictionApp beat -l INFO
 worker.3: celery -A PredictionApp worker -l INFO
+release: python manage.py migrate
 web: gunicorn FishWeightPrediction.wsgi
